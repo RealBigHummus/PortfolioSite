1d72bfcc07f24e4942ce6e0221ff195300ce7997
RealBigHummus <96791600+RealBigHummus@users.noreply.github.com>
2022-06-29 21:25:06 +0300

    Add files via upload

---
+++ PortfolioSite/Hod-Portfolio/script/script.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 2424;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
